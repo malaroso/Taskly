3496a469bf4ea5e35c11f88f12a72e178c7a4dae
Emirhan Ekin <ekinees@hotmail.com>
2025-02-20 11:47:31 +0300

    Crete navigation types

---
+++ src/types/navigation.ts
@@ -0,0 +1,14 @@
+
+export type RootStackParamList = {
+  Welcome: undefined;
+  Login: undefined;
+  Home: {
+    refresh?: boolean;
+  };
+  Profile: undefined;
+  Notifications: undefined;
+  TodoList: undefined;
+  ChangePassword: undefined;
+  FAQ: undefined;
+  About: undefined;
+}; 
