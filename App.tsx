b37a1a6d54561953cd8cc2282a42c9af34c086c5
Emirhan Ekin <ekinees@hotmail.com>
2025-02-20 11:42:19 +0300

    Added navigation and authprovider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AuthProvider } from './src/context/AuthContext';
+import AppNavigation from './navigation/appNavigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <AuthProvider>
+            <AppNavigation />
+        </AuthProvider>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
