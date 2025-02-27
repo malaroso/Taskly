89ff41cdee2a0e760a8dac0cc205b3ee64bb543c
Emirhan Ekin <ekinees@hotmail.com>
2025-02-27 17:42:57 +0300

    Update taskService added deletetask and comment function

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -44,4 +44,50 @@ export const addTask = async (formData: any) => {
         console.error('Error creating task:', error);
         throw error;
     }
+};
+
+export const deleteTask = async (taskID: number) => {
+    try {
+        const response = await axiosInstance.post('/deleteTask', { taskID });
+        return response.data;
+    } catch (error) {
+        console.error('Delete task error:', error);
+        throw error;
+    }
+};
+
+export const addComment = async (task_id: number, comment: string) => {
+    try {
+        const response = await axiosInstance.post('/addComments', {
+            task_id,
+            comment
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Add comment error:', error);
+        throw error;
+    }
+};
+
+export const deleteComment = async (comment_id: number) => {
+    try {
+        const response = await axiosInstance.post('/deleteComments', { comment_id });
+        return response.data;
+    } catch (error) {
+        console.error('Delete comment error:', error);
+        throw error;
+    }
+};
+
+export const updateComment = async (comment_id: number, comment: string) => {
+    try {
+        const response = await axiosInstance.post('/updateComments', {
+            comment_id,
+            comment
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Update comment error:', error);
+        throw error;
+    }
 }; 
